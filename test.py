b0854436ca639ce25d806372d020cdf85c235cde
xin <xin@scopemedia.com>
2018-04-04 16:33:38 -0700

    add test.py

---
+++ test.py
@@ -0,0 +1,32 @@
+from utils import Decoder, batch_inputs
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+def test_inputs():
+    img_decoder = Decoder(299, 'default', 'no_change')
+
+    with tf.Session() as sess:
+        batch_images, batch_labels = batch_inputs('train', 'dataset', 16, img_decoder)
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+        # for _ in range(10):
+
+            # images, ages, genders = sess.run([batch_images, batch_ages, batch_genders])
+            # print(ages)
+
+        images, labels = sess.run([batch_images, batch_labels])
+        coord.request_stop()
+        coord.join(threads)
+
+    fig = plt.figure()
+    print(images[0])
+    for i in range(len(images)):
+        fig.add_subplot(6, 6, i+1)
+        plt.axis('off')
+        plt.title("label: %s" % (np.argmax(labels[i]), ))
+        plt.imshow(np.clip(images[i], 0, 1))
+    plt.show()
+
+
+test_inputs()
